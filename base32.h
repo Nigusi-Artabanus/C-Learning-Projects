960932e75d36a64000dae0534d36bbe1b23d1598
Artabanus <89591183+Nigusi-Artabanus@users.noreply.github.com>
2026-07-13 17:27:07 +0000

    Added base32.h header file with function declarations for base32 encoding and decoding, along with error codes for incorrect padding and symbols.

---
+++ base32.h
@@ -0,0 +1,15 @@
+#ifndef BASE32_H
+#define BASE32_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define BASE32_ALPHABET "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+
+void base32_encode(const uint8_t *src, char * dst, size_t len);
+
+#define BASE32_INCORRECT_PADDING (-1)
+#define BASE32_INCORRECT_SYMBOL (-2)
+
+int base32_decode(const char *src, uint8_t *dst, size_t limit);
+#endif
