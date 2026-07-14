c3510921c274db3fa3a4733047525d1449d53193
Artabanus <89591183+Nigusi-Artabanus@users.noreply.github.com>
2026-07-14 18:10:42 +0000

    Base 32 encoder

--- TBOT_Password_Generator.cpp
+++ TBOT_Password_Generator.cpp
@@ -2,7 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 
+//#define SECRET "FRERT5GY76GHVV321DASSC4HB6TH67GH"
 int main() {
-
+    char src[] = "Hello";
+    char dst[80];
+    base32_encode((uint8_t *)src, dst, strlen(src));
+    printf("Encoded: %s\n", dst);
 return 0;
 }
