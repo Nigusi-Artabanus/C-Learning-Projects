6b41f86df8082a488a7bff6193533bf9a9ebf3dc
Artabanus <89591183+Nigusi-Artabanus@users.noreply.github.com>
2026-08-13 18:21:45 +0000

    Added time encooding

--- TBOTPasswordGenerator.cpp
+++ TBOTPasswordGenerator.cpp
@@ -1,17 +1,35 @@
 #include "base32.hpp"
+#include <bits/time.h>
+#include <time.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 
-#define SECRET "FRERT5GY76GHVV321DASSC4HB6TH67GH"
+#define SECRET "VXYLMUZAMMRWT5LFWBVDXCLYPJAMPZ5A"
+
+static void debug_binary(uint8_t *buf, size_t len){
+    for (int i = 0; i < 20; i++) {
+        printf("%02X", buf[i]);
+    }
+    putchar('\n'); 
+}
+static time_t unixepoch(){
+    struct timespec tp;
+    clock_gettime(CLOCK_REALTIME, &tp);
+    return tp.tv_sec;
+}
+
+static void encode_time_step(uint64_t step, uint8_t *dst){
+    for (int i = 0; i < 8; i++) {
+    dst[i] = (step >> (7 - i) * 8) & 0xFF;
+    }
+}
 int main() {
     uint8_t key[20];
     base32_decode(SECRET, key, sizeof(key));
-
-    for (int i = 0; i < sizeof(key); i++) {
-        printf("%02X", key[i]);
-    }
-
-    putchar('\n');
+    
+    uint64_t time_step = unixepoch() / 30;
+    printf("%ld\n", time_step);
     
     return 0;
 }
